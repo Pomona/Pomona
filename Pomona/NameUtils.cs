c6ca89d8c138eec1626daa5e36fdcc4e337c959d
Karsten Strand <karsten.strand@gmail.com>
2012-11-04 18:32:37 +0100

    Uris are now translated to plural form, like /critters instead of /critter (forgot to add files)

---
+++ Pomona/NameUtils.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+using System.Linq;
+
+namespace Pomona
+{
+    public static class NameUtils
+    {
+        public static string CapitalizeFirstLetter(string word)
+        {
+            if (word.Length == 0)
+                return word;
+            return word.Substring(0, 1).ToUpper() + word.Substring(1).ToLower();
+        }
+
+        public static string ConvertCamelCaseToUri(string word)
+        {
+            var parts = GetCamelCaseParts(word).ToArray();
+            return string.Join("-", parts.Select(x => x.ToLower()));
+        }
+
+        public static IEnumerable<string> GetCamelCaseParts(string camelCaseWord)
+        {
+            var startOfPart = 0;
+            for (var i = 0; i < camelCaseWord.Length; i++)
+            {
+                if (i > 0 && char.IsUpper(camelCaseWord[i]))
+                {
+                    yield return camelCaseWord.Substring(startOfPart, i - startOfPart);
+                    startOfPart = i;
+                }
+            }
+
+            if (startOfPart < camelCaseWord.Length)
+                yield return camelCaseWord.Substring(startOfPart);
+        }
+
+
+
+    }
+}
