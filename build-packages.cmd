a9def5b67529b43a96ef3c06b7ebf1f730598ca7
Karsten Nikolai Strand <karsten.strand@gmail.com>
2013-02-17 21:24:47 +0100

    Fixed build-packages.cmd, did not work after moving around of projects.

--- build-packages.cmd
+++ build-packages.cmd
@@ -4,5 +4,5 @@ echo "Building Nuget packages"
 
 
 
-.nuget\nuget.exe pack Pomona.Common\Pomona.Common.csproj -build -symbols -OutputDirectory build
-.nuget\nuget.exe pack Pomona\Pomona.csproj -build -symbols -OutputDirectory build
+.nuget\nuget.exe pack app\Pomona.Common\Pomona.Common.csproj -build -symbols -OutputDirectory build
+.nuget\nuget.exe pack app\Pomona\Pomona.csproj -build -symbols -OutputDirectory build
