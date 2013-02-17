78aa455ddcebc08f097038ee0bff3167242565b9
Karsten Nikolai Strand <karsten.strand@gmail.com>
2013-02-17 21:05:38 +0100

    Bumped version to 0.1.0-alpha7

--- app/Shared/CommonAssemblyInfo.cs
+++ app/Shared/CommonAssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.6")]
-[assembly: AssemblyInformationalVersion("0.1.0-alpha6")]
+[assembly: AssemblyFileVersion("0.1.0.7")]
+[assembly: AssemblyInformationalVersion("0.1.0-alpha7")]
 
 [assembly : ComVisible(false)]
