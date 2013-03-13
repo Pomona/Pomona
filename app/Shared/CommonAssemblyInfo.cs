4c6af61908668075bec9e1248573fc6619786c7c
Karsten Nikolai Strand <karsten.strand@okb.no>
2013-03-13 02:16:31 +0100

    Bumped version to 0.1.3-alpha5

--- app/Shared/CommonAssemblyInfo.cs
+++ app/Shared/CommonAssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.3.4")]
-[assembly: AssemblyInformationalVersion("0.1.3-alpha4")]
+[assembly: AssemblyFileVersion("0.1.3.5")]
+[assembly: AssemblyInformationalVersion("0.1.3-alpha5")]
 
 [assembly : ComVisible(false)]
