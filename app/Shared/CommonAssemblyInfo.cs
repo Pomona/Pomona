a71ba5ee8e50eddbf2c7522aec8b73678fd69d4f
Karsten Nikolai Strand <karsten.strand@gmail.com>
2013-05-08 11:04:21 +0200

    Small jsonbrowser.html fix. Bumped version to 0.1.7-alpha1

--- app/Shared/CommonAssemblyInfo.cs
+++ app/Shared/CommonAssemblyInfo.cs
@@ -41,6 +41,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 [assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.6.9")]
-[assembly: AssemblyInformationalVersion("0.1.6-alpha9")]
+[assembly: AssemblyFileVersion("0.1.7.1")]
+[assembly: AssemblyInformationalVersion("0.1.7-alpha1")]
 [assembly: ComVisible(false)]
