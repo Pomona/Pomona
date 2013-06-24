e9232f3ff211ab7d952c34edfd473fb5caf4fa12
Karsten Nikolai Strand <karsten.strand@gmail.com>
2013-06-24 16:36:18 +0200

    Bumped version to 0.1.8-alpha5

--- app/Shared/CommonAssemblyInfo.cs
+++ app/Shared/CommonAssemblyInfo.cs
@@ -41,6 +41,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 [assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.8.4")]
-[assembly: AssemblyInformationalVersion("0.1.8-alpha4")]
+[assembly: AssemblyFileVersion("0.1.8.5")]
+[assembly: AssemblyInformationalVersion("0.1.8-alpha5")]
 [assembly: ComVisible(false)]
