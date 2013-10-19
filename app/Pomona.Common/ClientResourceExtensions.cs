d2324d2d73c2e0e62a608250105bf3f7f3489a5f
Karsten Nikolai Strand <karsten.strand@gmail.com>
2013-10-19 20:48:48 +0200

    Added files, forgot to add in last commit.

---
+++ app/Pomona.Common/ClientResourceExtensions.cs
@@ -0,0 +1,49 @@
+﻿#region License
+
+// ----------------------------------------------------------------------------
+// Pomona source code
+// 
+// Copyright © 2013 Karsten Nikolai Strand
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a 
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+// ----------------------------------------------------------------------------
+
+#endregion
+
+using System.Collections.Generic;
+using Pomona.Common.Proxies;
+
+namespace Pomona.Common
+{
+    public static class ClientResourceExtensions
+    {
+        public static bool IsLoaded<T>(this IEnumerable<T> collection)
+            where T : IClientResource
+        {
+            var lazyProxy = collection as ILazyProxy;
+            return lazyProxy == null || lazyProxy.IsLoaded;
+        }
+
+        public static bool IsLoaded(this IClientResource resource)
+        {
+            var lazyProxy = resource as ILazyProxy;
+            return lazyProxy == null || lazyProxy.IsLoaded;
+        }
+    }
+}
