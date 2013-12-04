b528d4d96ed2b4e42529280e3a0829d90fb69654
Erik Hvattum <erik.hvattum@gmail.com>
2013-12-04 11:35:58 +0100

    fixed ref to CommonAssemblyInfo for TestingClient, removed bad character in build-packages.

--- build-packages.cmd
+++ build-packages.cmd
@@ -9,4 +9,4 @@ mkdir build
 .nuget\nuget.exe pack app\Pomona.Common\Pomona.Common.csproj -build -symbols -OutputDirectory build
 .nuget\nuget.exe pack app\Pomona\Pomona.csproj -build -symbols -OutputDirectory build
 .nuget\nuget.exe pack tests\Pomona.TestHelpers\Pomona.TestHelpers.csproj -build -symbols -OutputDirectory build
-.nuget\nuget.exe pack tests\Pomona.TestingClient\Pomona.TestingClient.csproj -build -symbols -OutputDirectory build
+.nuget\nuget.exe pack tests\Pomona.TestingClient\Pomona.TestingClient.csproj -build -symbols -OutputDirectory build
