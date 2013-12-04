0902c364d5a06e8cb385b3dddecb16560feea15f
Erik Hvattum <erik.hvattum@gmail.com>
2013-12-04 10:32:17 +0100

    Added entry for TestingClient in build-packages.cmd

--- build-packages.cmd
+++ build-packages.cmd
@@ -9,3 +9,4 @@ mkdir build
 .nuget\nuget.exe pack app\Pomona.Common\Pomona.Common.csproj -build -symbols -OutputDirectory build
 .nuget\nuget.exe pack app\Pomona\Pomona.csproj -build -symbols -OutputDirectory build
 .nuget\nuget.exe pack tests\Pomona.TestHelpers\Pomona.TestHelpers.csproj -build -symbols -OutputDirectory build
+.nuget\nuget.exe pack app\TestClient\TestClient.csproj -build -symbols -OutputDirectory build
