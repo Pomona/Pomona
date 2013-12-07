0d880e46f52e9089dbda188c1132a399c152ba70
Karsten Nikolai Strand <karsten.strand@okb.no>
2013-12-07 15:47:21 +0100

    Added test for url encoding of paths.

---
+++ tests/Pomona.UnitTests/UriResolverTests.cs
@@ -0,0 +1,52 @@
+﻿#region License
+
+// ----------------------------------------------------------------------------
+// Pomona source code
+// 
+// Copyright © 2013 Karsten Nikolai Strand
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a 
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+// ----------------------------------------------------------------------------
+
+#endregion
+
+using Nancy;
+
+using NUnit.Framework;
+
+using Pomona.Example;
+using Pomona.Example.Models.Existence;
+
+namespace Pomona.UnitTests
+{
+    [TestFixture]
+    public class UriResolverTests
+    {
+        [Test]
+        public void GetUriFor_WithEntityWithSpaceInPath_EncodesUrlTheRightWay()
+        {
+            var typeMapper = new TypeMapper(new CritterPomonaConfiguration());
+            var uriResolver = new UriResolver(typeMapper,
+                new NancyContext() { Request = new Request("GET", new Url() { BasePath = "/", HostName = "whateva" }) });
+            var galaxy = new Galaxy() { Name = "this is it" };
+            var url = uriResolver.GetUriFor(galaxy);
+            Assert.That(url, Is.EqualTo("http://whateva/galaxies/this%20is%20it"));
+        }
+    }
+}
