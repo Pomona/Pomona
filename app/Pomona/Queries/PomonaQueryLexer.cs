73b3425e7ad0748074d4ea2bcdddd45287883344
Karsten Nikolai Strand <karsten.strand@okb.no>
2013-12-17 10:09:43 +0100

    Remove timestamp from parser and lexer files, to avoid having them committed every time they are regenerated.

--- app/Pomona/Queries/PomonaQueryLexer.cs
+++ app/Pomona/Queries/PomonaQueryLexer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-// $ANTLR 3.5.0.2 Queries\\PomonaQuery.g 2013-12-03 16:22:09
+// $ANTLR 3.5.0.2 Queries\\PomonaQuery.g
 
 // The variable 'variable' is assigned but its value is never used.
 #pragma warning disable 219
