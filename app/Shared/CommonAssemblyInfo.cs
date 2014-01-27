e4f0057164b3a7f667078dc7f6ce637c7a19239f
Karsten Nikolai Strand <karsten.strand@okb.no>
2014-01-27 14:36:04 +0100

    Bumped version to 0.6.7-alpha2.

--- app/Shared/CommonAssemblyInfo.cs
+++ app/Shared/CommonAssemblyInfo.cs
@@ -41,6 +41,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 [assembly: AssemblyVersion("0.6.7.0")]
-[assembly: AssemblyFileVersion("0.6.7.1")]
-[assembly: AssemblyInformationalVersion("0.6.7-alpha1")]
+[assembly: AssemblyFileVersion("0.6.7.2")]
+[assembly: AssemblyInformationalVersion("0.6.7-alpha2")]
 [assembly: ComVisible(false)]
