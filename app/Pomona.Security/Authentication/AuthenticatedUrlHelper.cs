57d3c6499019eec2c0b8c2210dc8a252c2c0365d
Karsten Nikolai Strand <karsten.strand@okb.no>
2014-03-14 10:52:22 +0100

    Added initial classes and interfaces for Pomona.Security. Refactoring to come.

---
+++ app/Pomona.Security/Authentication/AuthenticatedUrlHelper.cs
@@ -0,0 +1,114 @@
+#region License
+
+// ----------------------------------------------------------------------------
+// Pomona source code
+// 
+// Copyright © 2014 Karsten Nikolai Strand
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a 
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+// ----------------------------------------------------------------------------
+
+#endregion
+
+using System;
+
+using Pomona.Common;
+
+namespace Pomona.Security.Authentication
+{
+    public class AuthenticatedUrlHelper
+    {
+        private readonly CryptoSerializer cryptoSerializer;
+
+
+        public AuthenticatedUrlHelper(CryptoSerializer cryptoSerializer)
+        {
+            if (cryptoSerializer == null)
+                throw new ArgumentNullException("cryptoSerializer");
+            this.cryptoSerializer = cryptoSerializer;
+        }
+
+
+        public static string AddQueryParameterString(string url, string key, string value)
+        {
+            var uri = new Uri(url);
+
+            // this gets all the query string key value pairs as a collection
+            var newQueryString = HttpUtility.ParseQueryString(uri.Query);
+
+            // this removes the key if exists
+            newQueryString.Add(key, value);
+
+            // this gets the page path from root without QueryString
+            var pagePathWithoutQueryString = uri.GetLeftPart(UriPartial.Path);
+
+            return newQueryString.Count > 0
+                ? String.Format("{0}?{1}", pagePathWithoutQueryString, newQueryString)
+                : pagePathWithoutQueryString;
+        }
+
+
+        public static string RemoveQueryStringByKey(string url, string key)
+        {
+            var uri = new Uri(url);
+
+            // this gets all the query string key value pairs as a collection
+            var newQueryString = HttpUtility.ParseQueryString(uri.Query);
+
+            // this removes the key if exists
+            newQueryString.Remove(key);
+
+            // this gets the page path from root without QueryString
+            var pagePathWithoutQueryString = uri.GetLeftPart(UriPartial.Path);
+
+            return newQueryString.Count > 0
+                ? String.Format("{0}?{1}", pagePathWithoutQueryString, newQueryString)
+                : pagePathWithoutQueryString;
+        }
+
+
+        public virtual DateTime OnGetUtcNow()
+        {
+            return DateTime.UtcNow;
+        }
+
+
+        public string CreatePreAuthorizedUrl(string urlString, DateTime? expiration = null)
+        {
+            // Path and query is part of verified url
+            var url = new Uri(urlString);
+            var verifiedUrlPart = url.PathAndQuery;
+            var urlToken = new UrlToken() { PathQueryHash = verifiedUrlPart, Expiration = expiration };
+            var tokenParameter = this.cryptoSerializer.SerializeEncryptedHexString(urlToken);
+            return AddQueryParameterString(urlString, "$token", tokenParameter);
+        }
+
+
+        public bool VerifyPreAuthorizedUrl(string urlString)
+        {
+            var query = HttpUtility.ParseQueryString(new Uri(urlString).Query);
+            var tokenParameter = query.Get("$token");
+            var urlToken = this.cryptoSerializer.DeserializeEncryptedHexString<UrlToken>(tokenParameter);
+            var urlWithoutToken = new Uri(RemoveQueryStringByKey(urlString, "$token"));
+            if (urlToken.Expiration.HasValue && urlToken.Expiration < OnGetUtcNow())
+                return false;
+            return urlToken.PathQueryHash == urlWithoutToken.PathAndQuery;
+        }
+    }
+}
---
+++ app/Pomona.Security/Authentication/CryptoSerializer.cs
@@ -0,0 +1,179 @@
+#region License
+
+// ----------------------------------------------------------------------------
+// Pomona source code
+// 
+// Copyright © 2014 Karsten Nikolai Strand
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a 
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+// ----------------------------------------------------------------------------
+
+#endregion
+
+using System;
+using System.IO;
+using System.IO.Compression;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Security.Cryptography;
+using System.Text;
+
+using Newtonsoft.Json;
+
+namespace Pomona.Security.Authentication
+{
+    public class CryptoSerializer
+    {
+        private readonly ISiteKeyProvider siteKeyProvider;
+
+        private bool compressionEnabled = true;
+
+
+        public CryptoSerializer(ISiteKeyProvider siteKeyProvider)
+        {
+            this.siteKeyProvider = siteKeyProvider;
+        }
+
+
+        private byte[] KeyBytes
+        {
+            get { return this.siteKeyProvider.SiteKey; }
+        }
+
+
+        public T DeserializeEncryptedHexString<T>(string hexString)
+        {
+            var encryptedTokenBytes = DecodeHexBytes<T>(hexString);
+            using (var codec = GetCodec())
+            {
+                var blockSizeBytes = codec.BlockSize / 8; // Should be 128/8 = 16 bytes
+                var iv = new byte[blockSizeBytes];
+                Array.Copy(encryptedTokenBytes, iv, blockSizeBytes);
+                codec.IV = iv;
+                var cipherTextLength = encryptedTokenBytes.Length - blockSizeBytes;
+
+                using (var decryptor = codec.CreateDecryptor())
+                {
+                    var plainBytes = decryptor.TransformFinalBlock(encryptedTokenBytes,
+                        blockSizeBytes
+                        /* skip iv bytes */,
+                        cipherTextLength);
+                    using (var memStream = new MemoryStream(plainBytes))
+                    {
+                        using (var gzipStream = CreateDecompressStream(memStream))
+                        {
+                            using (var textReader = new StreamReader(gzipStream, Encoding.UTF8))
+                                //using (var jsonReader = new JsonTextReader(textReader))
+                            {
+                                var str = textReader.ReadToEnd();
+                                return
+                                    JsonSerializer.CreateDefault(new JsonSerializerSettings()).Deserialize<T>(
+                                        new JsonTextReader(new StringReader(str)));
+                                //return JsonSerializer.CreateDefault(new JsonSerializerSettings()).Deserialize<T>(jsonReader);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+        public string SerializeEncryptedHexString(object obj)
+        {
+            using (var memStream = new MemoryStream())
+            {
+                using (var gzipStream = CreateCompressStream(memStream))
+                {
+                    using (var textWriter = new StreamWriter(gzipStream))
+                    {
+                        JsonSerializer.CreateDefault(new JsonSerializerSettings() { Formatting = Formatting.None })
+                            .Serialize(
+                                textWriter,
+                                obj);
+                        textWriter.Flush();
+                        gzipStream.Flush();
+                    }
+                }
+                using (var codec = GetCodec())
+                {
+                    codec.GenerateIV();
+
+                    //codec.IV = Enumerable.Repeat((byte)99, codec.BlockSize / 8).ToArray();
+                    using (var encryptor = codec.CreateEncryptor())
+                    {
+                        var plainBytes = memStream.ToArray();
+                        var encryptedMessageBytes = encryptor.TransformFinalBlock(plainBytes, 0, plainBytes.Length);
+                        var ivAndEncryptedMessageBytes = new byte[codec.IV.Length + encryptedMessageBytes.Length];
+                        Array.Copy(codec.IV, ivAndEncryptedMessageBytes, codec.IV.Length);
+                        Array.Copy(encryptedMessageBytes,
+                            0,
+                            ivAndEncryptedMessageBytes,
+                            codec.IV.Length,
+                            encryptedMessageBytes.Length);
+                        return EncodeHexBytes(ivAndEncryptedMessageBytes);
+                    }
+                }
+            }
+        }
+
+
+        private static byte[] DecodeHexBytes<T>(string hexString)
+        {
+            // Url-safe base64
+            return Convert.FromBase64String(hexString.Replace('-', '+').Replace('_', '/').Replace('.', '='));
+            return SoapHexBinary.Parse(hexString).Value;
+        }
+
+
+        private static string EncodeHexBytes(byte[] ivAndEncryptedMessageBytes)
+        {
+            return Convert.ToBase64String(ivAndEncryptedMessageBytes).Replace('+', '-').Replace('/', '_').Replace('=',
+                '.');
+        }
+
+
+        private Stream CreateCompressStream(MemoryStream memStream)
+        {
+            if (this.compressionEnabled)
+                return new DeflateStream(memStream, CompressionMode.Compress);
+            return memStream;
+        }
+
+
+        private Stream CreateDecompressStream(MemoryStream memStream)
+        {
+            if (this.compressionEnabled)
+                return new DeflateStream(memStream, CompressionMode.Decompress);
+            return memStream;
+        }
+
+
+        private SymmetricAlgorithm GetCodec()
+        {
+            var codec = Rijndael.Create();
+            if (codec == null)
+                throw new InvalidOperationException("What? Should not get a null crypto codec!");
+
+            codec.Key = KeyBytes;
+            codec.Mode = CipherMode.CFB; // For arbitrary lenghts
+            codec.Padding = PaddingMode.None;
+            codec.FeedbackSize = 8;
+            return codec;
+        }
+    }
+}
