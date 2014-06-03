bc011100e731b4d0e2b5a3f527a12617a1f616de
Karsten Nikolai Strand <karsten.strand@okb.no>
2014-06-03 10:23:53 +0200

    Removd NHibernateTests test project, it was never really supposed to be part of the solution.

--- tests/Pomona.NHibernateTests/Models/EntityAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PomonaNHibernateTest.Models
-{
-    public class EntityAttribute
-    {
-        public virtual int Id { get; set; }
-        public virtual string Key { get; set; }
-        public virtual string Value { get; set; }
-    }
-}
