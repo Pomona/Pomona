b5f2624e13abb350cb569d12c0269d7af862c91e
Jarl Rasmussen <jarl@okb.no>
2014-06-19 09:36:06 +0200

    Pump up the release

--- app/Shared/CommonAssemblyInfo.cs
+++ app/Shared/CommonAssemblyInfo.cs
@@ -41,6 +41,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 [assembly: AssemblyVersion("0.7.12.1")]
-[assembly: AssemblyFileVersion("0.7.16.1")]
-[assembly: AssemblyInformationalVersion("0.7.16")]
+[assembly: AssemblyFileVersion("0.7.17.1")]
+[assembly: AssemblyInformationalVersion("0.7.17")]
 [assembly: ComVisible(false)]
