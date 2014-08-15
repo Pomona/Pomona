9d5034c795f48b101aa2d2d0fb48af9593ee56b8
Karsten Nikolai Strand <karsten.strand@okb.no>
2014-08-15 14:37:23 +0200

    Bumped version to 0.9.1

--- app/Shared/CommonAssemblyInfo.cs
+++ app/Shared/CommonAssemblyInfo.cs
@@ -40,7 +40,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.9.0.1")]
-[assembly: AssemblyFileVersion("0.9.0.1")]
-[assembly: AssemblyInformationalVersion("0.9.0")]
+[assembly: AssemblyVersion("0.9.1.1")]
+[assembly: AssemblyFileVersion("0.9.1.1")]
+[assembly: AssemblyInformationalVersion("0.9.1")]
 [assembly: ComVisible(false)]
