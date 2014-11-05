cf4d6c1371634c8e37b838468757c66d8e113ec4
Karsten Nikolai Strand <karsten.strand@okb.no>
2014-11-05 19:06:19 +0100

    Added NhQueryProviderCapabilityResolver for querying NH3 about what properties are mapped (for now).

---
+++ tests/Pomona.NHibernate3.Tests/Models/Order.cs
@@ -0,0 +1,106 @@
+﻿#region License
+
+// ----------------------------------------------------------------------------
+// Pomona source code
+// 
+// Copyright © 2014 Karsten Nikolai Strand
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a 
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+// ----------------------------------------------------------------------------
+
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Pomona.NHibernate3.Tests.Models
+{
+    public class OrderLine
+    {
+        private Order order;
+
+
+        public OrderLine(Order order)
+        {
+            if (order == null)
+                throw new ArgumentNullException("order");
+            this.order = order;
+        }
+
+
+        protected OrderLine()
+        {
+        }
+
+
+        public virtual string Description { get; set; }
+        public virtual int Id { get; protected set; }
+
+        public virtual Order Order
+        {
+            get { return this.order; }
+            protected set { this.order = value; }
+        }
+    }
+
+    public class Order
+    {
+        public Order()
+        {
+            Lines = new List<OrderLine>();
+        }
+
+
+        public virtual int Id { get; protected set; }
+        public virtual IList<OrderLine> Lines { get; protected set; }
+
+        public virtual IEnumerable<OrderLine> LinesWithOddIds
+        {
+            get { return Lines.Where(x => x.Id % 2 == 1); }
+        }
+
+        public virtual string Reference { get; set; }
+    }
+
+    public class Customer
+    {
+        private string name;
+
+
+        public Customer(string name)
+        {
+            this.name = name;
+        }
+
+
+        protected Customer()
+        {
+        }
+
+
+        public virtual int Id { get; set; }
+
+        public virtual string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+    }
+}
---
+++ tests/Pomona.NHibernate3.Tests/NhQueryProviderCapabilityResolverTests.cs
@@ -0,0 +1,84 @@
+﻿#region License
+
+// ----------------------------------------------------------------------------
+// Pomona source code
+// 
+// Copyright © 2014 Karsten Nikolai Strand
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a 
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+// ----------------------------------------------------------------------------
+
+#endregion
+
+using FluentNHibernate.Automapping;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db.CsharpSqlite;
+
+using NHibernate;
+using NHibernate.Tool.hbm2ddl;
+
+using NUnit.Framework;
+
+using Pomona.NHibernate3.Tests.Mapping;
+using Pomona.NHibernate3.Tests.Models;
+
+namespace Pomona.NHibernate3.Tests
+{
+    [TestFixture]
+    public class NhQueryProviderCapabilityResolverTests
+    {
+        #region Setup/Teardown
+
+        [TestFixtureSetUp]
+        public void FixtureSetUp()
+        {
+            this.sessionFactory = Fluently.Configure()
+                .Database(CsharpSqliteConfiguration.Standard.InMemory)
+                .Mappings(m => m.AutoMappings.Add(AutoMap.AssemblyOf<Order>(new AutomappingConfiguration())))
+                .ExposeConfiguration(cfg => new SchemaExport(cfg).Create(true, true))
+                .BuildSessionFactory();
+        }
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.resolver = new NhQueryProviderCapabilityResolver(this.sessionFactory);
+        }
+
+        #endregion
+
+        private NhQueryProviderCapabilityResolver resolver;
+        private ISessionFactory sessionFactory;
+
+
+        [Test]
+        public void PropertyIsMapped_With_Property_Mapped_By_Nhibernate_Returns_False()
+        {
+            Assert.That(this.resolver.PropertyIsMapped(typeof(Order).GetProperty("Lines")), Is.True);
+        }
+
+
+        [Test]
+        public void PropertyIsMapped_With_Property_Not_Mapped_By_Nhibernate_Returns_False()
+        {
+            Assert.That(this.resolver.PropertyIsMapped(typeof(Order).GetProperty("LinesWithOddIds")), Is.False);
+        }
+    }
+}
