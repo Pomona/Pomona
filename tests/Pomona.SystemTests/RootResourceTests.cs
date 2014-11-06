f665e27adb55a8e4e717bf68fc122212d6398899
Karsten Nikolai Strand <karsten.strand@okb.no>
2014-11-06 11:01:42 +0100

    Added test for getting of root resource.

---
+++ tests/Pomona.SystemTests/RootResourceTests.cs
@@ -0,0 +1,47 @@
+﻿#region License
+
+// ----------------------------------------------------------------------------
+// Pomona source code
+// 
+// Copyright © 2014 Karsten Nikolai Strand
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a 
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+// ----------------------------------------------------------------------------
+
+#endregion
+
+using System.Collections.Generic;
+
+using NUnit.Framework;
+
+using Pomona.Common.Linq;
+
+namespace Pomona.SystemTests
+{
+    [TestFixture]
+    public class RootResourceTests : ClientTestsBase
+    {
+        [Test]
+        public void Get_Root_Returns_Dictionary_Containing_Resource_Links()
+        {
+            var dict = Client.Get<Dictionary<string, string>>("http://test/");
+            Assert.That(dict, Has.Member(new KeyValuePair<string, string>("critters", "http://test/critters")));
+        }
+    }
+}
