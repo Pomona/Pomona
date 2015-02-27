162b9b34c5baf461f7c7d6759fe1c59b8c4594c1
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2015-02-27 10:26:58 +0100

    Made the method and data parameters in WebClientRequestMessage's constructor optional.

--- app/Pomona.Common/Web/WebClientRequestMessage.cs
+++ app/Pomona.Common/Web/WebClientRequestMessage.cs
@@ -1,7 +1,9 @@
-﻿// ----------------------------------------------------------------------------
+﻿#region License
+
+// ----------------------------------------------------------------------------
 // Pomona source code
 // 
-// Copyright © 2013 Karsten Nikolai Strand
+// Copyright © 2014 Karsten Nikolai Strand
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a 
 // copy of this software and associated documentation files (the "Software"),
@@ -22,7 +24,8 @@
 // DEALINGS IN THE SOFTWARE.
 // ----------------------------------------------------------------------------
 
-using System.Collections.Generic;
+#endregion
+
 using System.Text;
 
 namespace Pomona.Common.Web
@@ -33,57 +36,57 @@ namespace Pomona.Common.Web
         private readonly HttpHeaders headers = new HttpHeaders();
         private readonly string method;
         private string uri;
-        private readonly string protocolVersion = "1.1";
 
-        public override string ToString()
-        {
-            var sb = new StringBuilder();
-            sb.AppendFormat("{0} {1} HTTP/{2}\r\n", method, uri, protocolVersion);
-            foreach (var h in headers)
-            {
-                foreach (var v in h.Value)
-                    sb.AppendFormat("{0}: {1}\r\n", h.Key, v);
-            }
-                sb.AppendLine();
-            if (data != null)
-            {
-                sb.Append(Encoding.UTF8.GetString(data));
-            }
-            sb.AppendLine();
-            return sb.ToString();
-        }
 
-        public string ProtocolVersion
-        {
-            get { return protocolVersion; }
-        }
-
-        public WebClientRequestMessage(string uri, byte[] data, string method)
+        public WebClientRequestMessage(string uri, byte[] data = null, string method = null)
         {
             this.uri = uri;
             this.data = data;
             this.method = method;
         }
 
-        public HttpHeaders Headers
-        {
-            get { return headers; }
-        }
-
-        public string Uri
-        {
-            get { return uri; }
-            set { uri = value; }
-        }
 
         public byte[] Data
         {
-            get { return data; }
+            get { return this.data; }
+        }
+
+        public HttpHeaders Headers
+        {
+            get { return this.headers; }
         }
 
         public string Method
         {
-            get { return method; }
+            get { return this.method; }
+        }
+
+        public string ProtocolVersion
+        {
+            get { return "1.1"; }
+        }
+
+        public string Uri
+        {
+            get { return this.uri; }
+            set { this.uri = value; }
+        }
+
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendFormat("{0} {1} HTTP/{2}\r\n", this.method, this.uri, ProtocolVersion);
+            foreach (var h in this.headers)
+            {
+                foreach (var v in h.Value)
+                    sb.AppendFormat("{0}: {1}\r\n", h.Key, v);
+            }
+            sb.AppendLine();
+            if (this.data != null)
+                sb.Append(Encoding.UTF8.GetString(this.data));
+            sb.AppendLine();
+            return sb.ToString();
         }
     }
 }
