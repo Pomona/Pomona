e150578055a18e1ccccb37aa7382e58c89e0bf6c
Karsten Nikolai Strand <karsten.strand@okb.no>
2015-03-05 13:46:00 +0100

    Bumped version to 0.17.4

--- app/Shared/CommonAssemblyInfo.cs
+++ app/Shared/CommonAssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 [assembly : AssemblyProduct("Pomona")]
 [assembly : AssemblyCopyright("Copyright © Karsten N. Strand 2015")]
 [assembly : AssemblyVersion("0.17.0.0")]
-[assembly : AssemblyFileVersion("0.17.3.0")]
-[assembly : AssemblyInformationalVersion("0.17.3")]
+[assembly : AssemblyFileVersion("0.17.4.0")]
+[assembly : AssemblyInformationalVersion("0.17.4")]
 [assembly : ComVisible(false)]
