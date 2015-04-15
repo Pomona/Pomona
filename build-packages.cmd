cff20e7741a7f1542cd98cdaae197e331a425a24
karsten <karsten.strand@okb.no>
2015-04-15 09:20:04 +0200

    Fix build-packages.cmd, use nuget command available on PATH.

--- build-packages.cmd
+++ build-packages.cmd
@@ -6,12 +6,12 @@ del build\*.nupkg
 mkdir build
 
 
-.nuget\nuget.exe pack app\Pomona.Common\Pomona.Common.csproj -build -symbols -OutputDirectory build
-.nuget\nuget.exe pack app\Pomona\Pomona.csproj -build -symbols -OutputDirectory build
-.nuget\nuget.exe pack app\Pomona.Security\Pomona.Security.csproj -build -symbols -OutputDirectory build
-.nuget\nuget.exe pack app\Pomona.Scheduler\Pomona.Scheduler.csproj -build -symbols -OutputDirectory build
-.nuget\nuget.exe pack app\Pomona.Profiling.MiniProfiler\Pomona.Profiling.MiniProfiler.csproj -build -symbols -OutputDirectory build
-.nuget\nuget.exe pack app\Pomona.NHibernate3\Pomona.NHibernate3.csproj -build -symbols -OutputDirectory build
-.nuget\nuget.exe pack app\Pomona.NHibernate4\Pomona.NHibernate4.csproj -build -symbols -OutputDirectory build
-.nuget\nuget.exe pack tests\Pomona.TestHelpers\Pomona.TestHelpers.csproj -build -symbols -OutputDirectory build
-.nuget\nuget.exe pack tests\Pomona.TestingClient\Pomona.TestingClient.csproj -build -symbols -OutputDirectory build
+nuget pack app\Pomona.Common\Pomona.Common.csproj -build -symbols -OutputDirectory build
+nuget pack app\Pomona\Pomona.csproj -build -symbols -OutputDirectory build
+nuget pack app\Pomona.Security\Pomona.Security.csproj -build -symbols -OutputDirectory build
+nuget pack app\Pomona.Scheduler\Pomona.Scheduler.csproj -build -symbols -OutputDirectory build
+nuget pack app\Pomona.Profiling.MiniProfiler\Pomona.Profiling.MiniProfiler.csproj -build -symbols -OutputDirectory build
+nuget pack app\Pomona.NHibernate3\Pomona.NHibernate3.csproj -build -symbols -OutputDirectory build
+nuget pack app\Pomona.NHibernate4\Pomona.NHibernate4.csproj -build -symbols -OutputDirectory build
+nuget pack tests\Pomona.TestHelpers\Pomona.TestHelpers.csproj -build -symbols -OutputDirectory build
+nuget pack tests\Pomona.TestingClient\Pomona.TestingClient.csproj -build -symbols -OutputDirectory build
