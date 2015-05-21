2a90a725a01dece1126cb8c2554529ca5fe26965
karsten <karsten.strand@okb.no>
2015-05-21 09:13:10 +0200

    Avoid generating symbols for nuget packages, as symbol uploading has for some reason stopped working on build server.

--- build-packages.cmd
+++ build-packages.cmd
@@ -6,12 +6,12 @@ del build\*.nupkg
 mkdir build
 
 
-nuget pack -Prop Configuration=Release app\Pomona.Common\Pomona.Common.csproj -build -symbols -OutputDirectory build
-nuget pack -Prop Configuration=Release app\Pomona\Pomona.csproj -build -symbols -OutputDirectory build
-nuget pack -Prop Configuration=Release app\Pomona.Security\Pomona.Security.csproj -build -symbols -OutputDirectory build
-nuget pack -Prop Configuration=Release app\Pomona.Scheduler\Pomona.Scheduler.csproj -build -symbols -OutputDirectory build
-nuget pack -Prop Configuration=Release app\Pomona.Profiling.MiniProfiler\Pomona.Profiling.MiniProfiler.csproj -build -symbols -OutputDirectory build
-nuget pack -Prop Configuration=Release app\Pomona.NHibernate3\Pomona.NHibernate3.csproj -build -symbols -OutputDirectory build
-nuget pack -Prop Configuration=Release app\Pomona.NHibernate4\Pomona.NHibernate4.csproj -build -symbols -OutputDirectory build
-nuget pack -Prop Configuration=Release tests\Pomona.TestHelpers\Pomona.TestHelpers.csproj -build -symbols -OutputDirectory build
-nuget pack -Prop Configuration=Release tests\Pomona.TestingClient\Pomona.TestingClient.csproj -build -symbols -OutputDirectory build
+nuget pack -Prop Configuration=Release app\Pomona.Common\Pomona.Common.csproj -build -OutputDirectory build
+nuget pack -Prop Configuration=Release app\Pomona\Pomona.csproj -build -OutputDirectory build
+nuget pack -Prop Configuration=Release app\Pomona.Security\Pomona.Security.csproj -build -OutputDirectory build
+nuget pack -Prop Configuration=Release app\Pomona.Scheduler\Pomona.Scheduler.csproj -build -OutputDirectory build
+nuget pack -Prop Configuration=Release app\Pomona.Profiling.MiniProfiler\Pomona.Profiling.MiniProfiler.csproj -build -OutputDirectory build
+nuget pack -Prop Configuration=Release app\Pomona.NHibernate3\Pomona.NHibernate3.csproj -build -OutputDirectory build
+nuget pack -Prop Configuration=Release app\Pomona.NHibernate4\Pomona.NHibernate4.csproj -build -OutputDirectory build
+nuget pack -Prop Configuration=Release tests\Pomona.TestHelpers\Pomona.TestHelpers.csproj -build -OutputDirectory build
+nuget pack -Prop Configuration=Release tests\Pomona.TestingClient\Pomona.TestingClient.csproj -build -OutputDirectory build
