bd374a6215d44e2b9ab9df1f17d00dba16a4288d
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2015-08-05 14:23:48 +0200

    Added CommonAssemblyInfo to all projects and adjusted the AssemblyInfo files accordingly.

--- tests/Pomona.TestHelpers/Properties/AssemblyInfo.cs
+++ tests/Pomona.TestHelpers/Properties/AssemblyInfo.cs
@@ -30,14 +30,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-
 [assembly : AssemblyTitle("Pomona.TestHelpers")]
 [assembly : AssemblyDescription("Help library for tests")]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-
 [assembly : Guid("597652d0-b825-45f4-afd4-96bc448060ba")]
 [assembly : InternalsVisibleTo("Pomona.UnitTests")]
