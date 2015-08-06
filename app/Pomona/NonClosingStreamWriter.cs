84dc768829b8a6042122d1028ec4c92291698cf9
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2015-08-06 09:30:57 +0200

    Added documentation and field for clarity

--- app/Pomona/NonClosingStreamWriter.cs
+++ app/Pomona/NonClosingStreamWriter.cs
@@ -32,16 +32,34 @@ using System.Text;
 namespace Pomona
 {
     /// <summary>
-    /// A version of StreamWriter that doesn't close underlying stream when disposed.
+    /// A version of <see cref="StreamWriter"/> that doesn't close the underlying <see cref="Stream"/> when <see cref="Dispose"/>d.
+    /// Writes to the underlying <see cref="Stream"/> with a BOM-less UTF-8 encoding.
     /// </summary>
     internal class NonClosingStreamWriter : StreamWriter
     {
+        private static readonly Encoding bomLessUtf8Encoding;
+
+
+        /// <summary>Initializes the <see cref="NonClosingStreamWriter"/> class.</summary>
+        static NonClosingStreamWriter()
+        {
+            bomLessUtf8Encoding = new UTF8Encoding(false);
+        }
+
+
+        /// <summary>Initializes a new instance of the <see cref="NonClosingStreamWriter"/> class.</summary>
+        /// <param name="stream">The stream to write to.</param>
         public NonClosingStreamWriter(Stream stream)
-            : base(stream, new UTF8Encoding(false))
+            : base(stream, bomLessUtf8Encoding)
         {
         }
 
 
+        /// <summary>
+        /// Flushes the content of the <see cref="StreamWriter"/> to the underlying <see cref="Stream"/>,
+        /// but does not close it.
+        /// </summary>
+        /// <param name="disposing">true to release both managed and unmanaged resources; false to release only unmanaged resources.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing)
