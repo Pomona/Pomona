33b05d4bf8a7a783cce99d3fb75bc6b876761a61
karsten <karsten.strand@okb.no>
2016-03-14 11:25:52 +0100

    Fix package build script, to make it work with nuget3

--- build-packages.cmd
+++ build-packages.cmd
@@ -9,13 +9,13 @@ call "%VS140COMNTOOLS%\VsDevCmd.bat"
 
 msbuild Pomona.sln /target:Build /p:Configuration=Release
 
-nuget pack -Prop Configuration=Release app\Pomona.Common\Pomona.Common.csproj -build -OutputDirectory build
-nuget pack -Prop Configuration=Release app\Pomona\Pomona.csproj -build -OutputDirectory build
-nuget pack -Prop Configuration=Release app\Pomona.Nancy\Pomona.Nancy.csproj -build -OutputDirectory build
-nuget pack -Prop Configuration=Release app\Pomona.Security\Pomona.Security.csproj -build -OutputDirectory build
-nuget pack -Prop Configuration=Release app\Pomona.Scheduler\Pomona.Scheduler.csproj -build -OutputDirectory build
-nuget pack -Prop Configuration=Release app\Pomona.Profiling.MiniProfiler\Pomona.Profiling.MiniProfiler.csproj -build -OutputDirectory build
-nuget pack -Prop Configuration=Release app\Pomona.NHibernate3\Pomona.NHibernate3.csproj -build -OutputDirectory build
-nuget pack -Prop Configuration=Release app\Pomona.NHibernate4\Pomona.NHibernate4.csproj -build -OutputDirectory build
-nuget pack -Prop Configuration=Release tests\Pomona.TestHelpers\Pomona.TestHelpers.csproj -build -OutputDirectory build
-nuget pack -Prop Configuration=Release tests\Pomona.TestingClient\Pomona.TestingClient.csproj -build -OutputDirectory build
+nuget pack -Prop Configuration=Release app\Pomona.Common\Pomona.Common.csproj -OutputDirectory build
+nuget pack -Prop Configuration=Release app\Pomona\Pomona.csproj -OutputDirectory build
+nuget pack -Prop Configuration=Release app\Pomona.Nancy\Pomona.Nancy.csproj -OutputDirectory build
+nuget pack -Prop Configuration=Release app\Pomona.Security\Pomona.Security.csproj -OutputDirectory build
+nuget pack -Prop Configuration=Release app\Pomona.Scheduler\Pomona.Scheduler.csproj -OutputDirectory build
+nuget pack -Prop Configuration=Release app\Pomona.Profiling.MiniProfiler\Pomona.Profiling.MiniProfiler.csproj -OutputDirectory build
+nuget pack -Prop Configuration=Release app\Pomona.NHibernate3\Pomona.NHibernate3.csproj -OutputDirectory build
+nuget pack -Prop Configuration=Release app\Pomona.NHibernate4\Pomona.NHibernate4.csproj -OutputDirectory build
+nuget pack -Prop Configuration=Release tests\Pomona.TestHelpers\Pomona.TestHelpers.csproj -OutputDirectory build
+nuget pack -Prop Configuration=Release tests\Pomona.TestingClient\Pomona.TestingClient.csproj -OutputDirectory build
