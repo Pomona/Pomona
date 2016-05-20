15c54a583644d29b7407b7ebd623613fc10517ad
karsten <karsten.strand@okb.no>
2016-05-20 16:17:11 +0200

    Try to use temp directory for gh-pages worktree

--- update-docs.ps1
+++ update-docs.ps1
@@ -10,19 +10,20 @@ if ($(git rev-parse --abbrev-ref HEAD) -eq "develop")
 {
   git remote -v
   git fetch
-  git worktree add ..\Pomona-gh-pages gh-pages
-  pushd ..\Pomona-gh-pages
+  $TempDir = $env:temp + "\" + [System.Guid]::NewGuid().ToString()
+  git worktree add $TempDir gh-pages
+  pushd $TempDir
   git reset --hard origin/gh-pages
   popd
   nuget restore
-  .\packages\Storyteller.3.0.0.334-rc\tools\ST.exe doc-export ..\Pomona-gh-pages\preview\ FileDump -c .\samples\ .\tests\
-  pushd ..\Pomona-gh-pages
+  .\packages\Storyteller.3.0.0.334-rc\tools\ST.exe doc-export $TempDir\preview\ FileDump -c .\samples\ .\tests\
+  pushd $TempDir
   git status
   git add -A .
   git commit -m "Updated ST doc preview"
   git push
   popd
-  Remove-Item  ..\Pomona-gh-pages\ -Recurse -Force
+  Remove-Item  $TempDir\ -Recurse -Force
   git worktree prune
   Write-Output "Updated documentation"
 }
