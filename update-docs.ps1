dc8623f6a1b4b4e31fbaeeb1a2272eee8f05f342
karsten <karsten.strand@okb.no>
2016-05-23 11:13:32 +0200

    Specify repo when cloning gh-pages

--- update-docs.ps1
+++ update-docs.ps1
@@ -3,7 +3,7 @@ $ErrorActionPreference = "Stop"
 if ($(git rev-parse --abbrev-ref HEAD) -eq "develop")
 {
   $TempDir = $env:temp + "\" + [System.Guid]::NewGuid().ToString()
-  git clone git@github.com:Pomona/Pomona-gh-pages.git $TempDir
+  git clone git@github.com:Pomona/Pomona-gh-pages.git $TempDir -b gh-pages
   Write-Output "$TempDir"
   nuget restore
   .\packages\Storyteller.3.0.0.334-rc\tools\ST.exe doc-export $TempDir\preview\ FileDump -c .\samples\ .\tests\
