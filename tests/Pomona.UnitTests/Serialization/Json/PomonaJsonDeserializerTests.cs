ad24acba572041b96df3f05b017b0a9e5f9d1c93
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2017-09-21 17:34:14 +0200

    Updated assert

--- tests/Pomona.UnitTests/Serialization/Json/PomonaJsonDeserializerTests.cs
+++ tests/Pomona.UnitTests/Serialization/Json/PomonaJsonDeserializerTests.cs
@@ -86,7 +86,7 @@ namespace Pomona.UnitTests.Serialization.Json
                     this.deserializer.DeserializeString("\"blahrg\"", options : new DeserializeOptions() { ExpectedBaseType = typeof(bool) }));
 
             // This will wrap a JsonSerializationException for now.
-            Assert.That(ex.Message, Does.StartWith("Error converting value \"blahrg\" to type 'System.Boolean'."));
+            Assert.That(ex.Message, Does.StartWith("Could not convert string to boolean: blahrg"));
             Assert.That(ex.InnerException, Is.InstanceOf<JsonSerializationException>());
         }
 
